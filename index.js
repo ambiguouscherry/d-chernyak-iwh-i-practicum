162c4bb0c15f9b6c6740ef93a48ef662f3c1763a
ambiguouscherry <dz@zaptest.ai>
2025-12-05 14:16:23 -0800

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,11 +9,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const favoriteMusicalArtistsUrl = 'https://api.hubspot.com/crm/v3/objects/2-222140430';
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const propertyParams = 'properties=artist_name&properties=favorite_song&properties=genre';
+        const resp = await axios.get(`${favoriteMusicalArtistsUrl}?${propertyParams}`, { headers });
+
+        const data = resp.data.results || [];
+        const columns = ['artist_name', 'genre', 'favorite_song'];
+
+        res.render('homepage', {
+          title: 'Homepage | Integrating With HubSpot I Practicum',
+          data,
+          columns,
+         });
+
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Error fetching data from HubSpot API');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
